56240146e98fe1708afd17a9c04932ca4b203dc9
Sajjad Kafshgar <sajjad.kafshgar123@gmail.com>
2024-08-07 09:26:56 +0330

    js for portfolio

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,15 @@
     const navToggleicon = document.querySelector('.nav__toggle-icon')
     const menu = document.querySelector('.menu')
+    const resumeListItems = document.querySelectorAll('.resume-list__item')
 
     navToggleicon.addEventListener('click',function (){
       this.classList.toggle('nav__toggle-icon--open') 
       menu.classList.toggle('menu--open')
     })
+
+    resumeListItems.forEach(resumeListItem => {
+      resumeListItem.addEventListener('click',function (){
+          document.querySelector('.resume-list__item--show').classList.remove('resume-list__item--show')
+          this.classList.add('resume-list__item--show')
+      })
+  })
